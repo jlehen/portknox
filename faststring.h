73c053b6bf75e541b840f2fc2e2e49830d44b124
Jeremie Le Hen <jeremie@le-hen.org>
2009-05-21 21:54:06 +0000

    Add a few useful comments.

--- faststring.h
+++ faststring.h
@@ -8,12 +8,14 @@ typedef struct {
 	int maxlen;
 } faststring;
 
+/* Alloc size includes the final '\0'. */
 extern faststring *faststring_alloc(int);
 extern void faststring_free(faststring *);
 extern faststring *faststring_strcpy(faststring *, const char *);
 extern faststring *faststring_strncpy(faststring *, const char *, int);
 extern faststring *faststring_strcat(faststring *, const char *);
 extern faststring *faststring_strncat(faststring *, const char *, int);
+/* Frees the faststring wrapper struct. */
 extern char *faststring_export(faststring *);
 int faststring_strlen(const faststring *);
 
